ccfbd9bbfbdb2767985021b5b96d32d9d09bee6e
kjh9267 <kjh9267z@naver.com>
2018-08-07 18:55:24 +0900

    5549cd

---
+++ src/string_handing/BOJ_10809.java
@@ -0,0 +1,26 @@
+package string_handing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class BOJ_10809 {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int count[] = new int[26];
+		Arrays.fill(count,-1);
+		int cnt = -1;
+		
+		for(char word: br.readLine().toCharArray()) {
+			cnt++;
+			if(count[word - 97] == -1) {
+				count[word - 97] = cnt;
+			}
+		}
+		for(int i : count) {
+			System.out.print(i + " ");
+		}
+	}
+}
---
+++ src/string_handing/BOJ_11654.java
@@ -0,0 +1,14 @@
+package string_handing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_11654 {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char character = br.readLine().charAt(0);
+		
+		System.out.println(character + 0);
+	}
+}
