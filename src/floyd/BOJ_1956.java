199a9b5b1d4fa3229285e5efa770f2ef8ff01f92
kjh9267 <kjh9267z@naver.com>
2018-08-10 18:37:16 +0900

    Prime

---
+++ src/floyd/BOJ_1956.java
@@ -0,0 +1,53 @@
+package floyd;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1956 {
+
+	public static final int INF = 10_001;
+
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+
+		int[][] graph = new int[V + 1][V + 1];
+		int res = INF;
+
+		for (int i = 0; i < V + 1; i++) {
+			Arrays.fill(graph[i], INF);
+		}
+
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			graph[a][b] = c;
+		}
+
+		for (int k = 1; k < V + 1; k++) {
+			for (int i = 1; i < V + 1; i++) {
+				for (int j = 1; j < V + 1; j++) {
+					if (graph[i][j] > graph[i][k] + graph[k][j]) {
+						graph[i][j] = graph[i][k] + graph[k][j];
+					}
+				}
+			}
+		}
+		
+		for (int i = 1; i < V + 1; i++) {
+			if (res > graph[i][i]) {
+				res = graph[i][i];
+			}
+		}
+		System.out.println(res == INF ? -1 : res);
+	}
+}
---
+++ src/math/BOJ_1978.java
@@ -0,0 +1,46 @@
+package math;
+
+import java.awt.font.NumericShaper.Range;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1978 {
+	
+	public static void main(String args[]) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N];
+		int cnt = 0;
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		boolean[] isPrime = new boolean[1001];
+		Arrays.fill(isPrime, true);
+		isPrime[1] = false;
+		
+		for(int i = 4; i < 1001; i += 2) {
+			isPrime[i] = false;
+		}
+		
+		for(int i = 3; i < 1001; i++) {
+			for(int j = i*i; j < 1001; j += i*2) {
+				isPrime[j] = false;
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if(isPrime[nums[i]]) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
