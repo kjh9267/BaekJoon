a20c00173f736b7d5871cbabcf2e0bfea63ec46e
kjh9267 <kjh9267z@naver.com>
2018-12-29 19:36:48 +0900

    segment_tree

---
+++ src/segment_tree/BOJ_2357.java
@@ -0,0 +1,67 @@
+package segment_tree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2357 {
+	public static int N, M;
+	public static int[] min_tree, max_tree, nums;
+	
+	public static void main(String args[]) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int height = (int) Math.ceil(Math.log(N)/Math.log(2));
+		int size = (int) Math.pow(2, height + 1);
+		min_tree = new int[size];
+		max_tree = new int[size];
+		nums = new int[N+1];
+		
+		for(int i = 1; i < N+1; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		min_init(1,1,N);
+		max_init(1,1,N);
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(min(1,1,N,a,b)).append(" ").append(max(1,1,N,a,b)).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	public static int min_init(int node, int start, int end) {
+		if(start == end) return min_tree[node] = nums[start];
+		int mid = (start + end)/2;
+		return min_tree[node] = Math.min(min_init(node * 2, start, mid),
+				min_init(node * 2 + 1, mid + 1, end));
+	}
+	
+		public static int max_init(int node, int start, int end) {
+		if(start == end) return max_tree[node] = nums[start];
+		int mid = (start + end)/2;
+		return max_tree[node] = Math.max(max_init(node * 2, start, mid),
+				max_init(node * 2 + 1, mid + 1, end));
+	}
+	
+	public static int min(int node, int start, int end, int left, int right) {
+		if(left > end || right < start) return Integer.MAX_VALUE;
+		if(left <= start && end <= right) return min_tree[node];
+		int mid = (start + end)/2;
+		return Math.min(min(node * 2, start, mid, left, right),
+				min(node * 2 + 1, mid + 1, end, left, right));
+	}
+	
+	public static int max(int node, int start, int end, int left, int right) {
+		if(left > end || right < start) return Integer.MIN_VALUE;
+		if(left <= start && end <= right) return max_tree[node];
+		int mid = (start + end)/2;
+		return Math.max(max(node * 2, start, mid, left, right), 
+				max(node * 2 + 1, mid + 1, end, left, right));
+	}
+}
