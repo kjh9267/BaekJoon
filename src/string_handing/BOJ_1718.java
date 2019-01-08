5aca94e014cb65949d5a2d8c31e4cae98e48aeba
kjh9267 <kjh9267z@naver.com>
2019-01-08 16:46:00 +0900

    string_handling

---
+++ src/string_handing/BOJ_1718.java
@@ -0,0 +1,45 @@
+package string_handing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1718 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String normal = br.readLine();
+		String password = br.readLine();
+		
+		System.out.println(convert(normal, password));
+	}
+	
+	public static StringBuilder convert(String normal, String password) {
+		StringBuilder res = new StringBuilder();
+		int len = normal.length();
+		int plen = password.length();
+		int index = 0;
+		
+		for(int i = 0; i < len; i++) {
+			char x = normal.charAt(i);
+			char y = password.charAt(index);
+			
+			if(x == ' ') {
+				res.append(' ');
+				index = next_index(index, plen);
+				continue;
+			}
+			
+			int c = x - (y - 96);
+			
+			if (c < 97)
+				c = c - 97 + 123;
+				
+			res.append((char)c);
+			index = next_index(index, plen);
+		}
+		return res;
+	}
+	
+	public static int next_index(int index, int plen) {
+		return ++index == plen ? index - plen : index;
+	}
+}
