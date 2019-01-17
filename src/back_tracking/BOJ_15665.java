6e356ebeb80327cf1f8e0a961528167f68e38b27
kjh9267 <kjh9267z@naver.com>
2019-01-17 15:18:01 +0900

    backTracking

---
+++ src/back_tracking/BOJ_15665.java
@@ -0,0 +1,48 @@
+package back_tracking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15665 {
+	public static int[] nums;
+	public static int N, M;
+	public static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++)
+			nums[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(nums);
+
+		dfs(0, 0, "");
+		System.out.print(sb);
+	}
+
+	public static void dfs(int cur, int depth, String s) {
+		if(depth == M ) {
+			sb.append(s);
+			sb.append('\n');
+			return;
+		}
+		
+		if(cur == N)
+			return;
+		
+		int prev = 0;
+		for(int i = 0; i < N; i++) {
+			if(prev == nums[i])
+				continue;
+			prev = nums[i];
+			dfs(i, depth + 1, s + nums[i] + " ");
+		}
+	}
+}
