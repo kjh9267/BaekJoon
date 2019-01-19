959fff9a10ed2f9ac3c61ff25bc306d1086624b7
kjh9267 <kjh9267z@naver.com>
2019-01-19 16:50:03 +0900

    postfix notation

--- src/stack/BOJ_1918.java
+++ src/stack/BOJ_1918.java
@@ -7,66 +7,74 @@ import java.util.Stack;
 
 public class BOJ_1918 {
 	public static HashMap<Character, Integer> priority;
+	public static Stack<Character> stack;
+	public static StringBuilder sb;
 
 	public static void main(String[] args) throws Exception{
-		Stack<Character> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String expression = br.readLine();
-		int len = expression.length();
 		
 		init();
-		
-		for(int i = 0; i < len; i++) {
-			char token = expression.charAt(i);
-			
-			if(token == '(')
-				stack.push(token);
-			else if(token == '*' || token == '/' || token == '+' || token == '-') {
-				while(true) {
-					if(stack.isEmpty()) {
-						stack.push(token);
-						break;
-					}
-					char top = stack.peek();
-					int curPriority = priority.get(token);
-					int topPriority = priority.get(top);
-
-					if(curPriority > topPriority) {
-						stack.push(token);
-						break;
-					}
-					
-					sb.append(stack.pop());
-				}
-			}
-			else if(token == ')') {
-				while(true) {
-					char top = stack.pop();
-					if(top == '(')
-						break;
-					else
-						sb.append(top);
-				}
-			}
-			else {
-				sb.append(token);
-			}
-		}
-		len = stack.size();
-		while(len-- > 0) {
-			sb.append(stack.pop());
-		}
+		solve(expression);
 		
 		System.out.print(sb);
 	}
 	
 	public static void init() {
+		sb = new StringBuilder();
+		stack = new Stack<>();
 		priority = new HashMap<>();
 		priority.put('(', 0);
 		priority.put('-', 1);
 		priority.put('+', 1);
 		priority.put('*', 2);
 		priority.put('/', 2);
+	}
+	
+	public static void solve(String expression) {
+		int len = expression.length();
+		for(int i = 0; i < len; i++) {
+			char token = expression.charAt(i);
+			if(token == '(')
+				stack.push(token);
+			else if(token == '*' || token == '/' || token == '+' || token == '-')
+				operator(token);
+			else if(token == ')')
+				rightBracket();
+			else
+				sb.append(token);
+		}
+		len = stack.size();
+		while(len-- > 0) {
+			sb.append(stack.pop());
+		}
+	}
+	
+	public static void operator(char token) {
+		while(true) {
+			if(stack.isEmpty()) {
+				stack.push(token);
+				break;
+			}
+			char top = stack.peek();
+			int curPriority = priority.get(token);
+			int topPriority = priority.get(top);
+
+			if(curPriority > topPriority) {
+				stack.push(token);
+				break;
+			}
+			sb.append(stack.pop());
+		}
+	}
+	
+	public static void rightBracket() {
+		while(true) {
+			char top = stack.pop();
+			if(top == '(')
+				break;
+			else
+				sb.append(top);
+		}
 	}
 }
