8ed890b1ef9571b5dfef339dad7dfff5090ead22
kjh9267 <kjh9267z@naver.com>
2019-06-22 22:30:29 +0900

    divide_and_conquer

---
+++ src/divide_conquer/BOJ_1992.java
@@ -0,0 +1,56 @@
+package divide_conquer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author Junho
+ *
+ * @see https://www.acmicpc.net/problem/1992
+ *
+ */
+
+public class BOJ_1992 {
+	public static char[][] grid;
+	public static StringBuilder sb;
+	
+	public static void solve(int x, int y, int len) {
+		boolean flag = false;
+		for(int i = y; i < y + len; i++) {
+			for(int j = x; j < x + len; j++) {
+				if(grid[i][j] != grid[y][x]) {
+					flag = true;
+					break;
+				}
+			}
+			if(flag)
+				break;
+		}
+		
+		if(!flag) {
+			sb.append(grid[y][x]);
+			return;
+		}
+		
+		sb.append('(');
+		solve(x, y, len / 2);
+		solve(x + len / 2, y, len / 2);
+		solve(x, y + len / 2, len / 2);
+		solve(x + len / 2, y + len / 2, len / 2);
+		sb.append(')');
+	}
+		
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		grid = new char[N + 1][N + 1];
+		sb = new StringBuilder();
+				
+		for(int i = 0; i < N; i++)
+			grid[i] = br.readLine().toCharArray();
+		
+		solve(0, 0, N);
+		System.out.println(sb);
+	}
+}
