4abc8cf1215c8bb10b08ed6d26012a6f7f700c43
kjh9267 <kjh9267z@naver.com>
2019-06-24 13:58:04 +0900

    palindrome

---
+++ src/string_handing/BOJ_1259.java
@@ -0,0 +1,30 @@
+package string_handing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1259 {
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String num = br.readLine();
+			boolean key = true;
+			if(num.equals("0"))
+				break;
+			int len = num.length();
+			for(int i = 0; i < len / 2; i++) {
+				if(num.charAt(i) != num.charAt(len - 1 - i)) {
+					key = false;
+					break;
+				}
+			}
+			if(key)
+				sb.append("yes\n");
+			else
+				sb.append("no\n");
+		}
+		System.out.println(sb);
+	}
+}
