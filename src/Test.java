a4a6d7f24c753018f727f42eca5832384b01582b
kjh9267 <kjh9267z@naver.com>
2019-06-29 18:28:58 +0900

    2dimension

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * 
- * @author Junho
- *
- * @see https://www.acmicpc.net/problem/1436
- *
- */
 
 public class Test {
+	public static long[][] dp;
+	public static int[] acc, data;
+	
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
-		while(N-- > 0) {
+		while(T-- > 0) {
+			int K = Integer.parseInt(br.readLine());
+			acc = new int[K + 1];
+			dp = new long[K][K];
+			data = new int[K];
+			for(int i = 0; i < K; i++)
+				Arrays.fill(dp[i], -1);
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
 			
-			sb.append("You get ").append(a/b).append(" piece(s) and your dad gets ").append(a%b).append(" piece(s).").append('\n');
+			for(int i = 0; i < K; i++)
+				data[i] = Integer.parseInt(st.nextToken());
+			for(int i = 1; i < K + 1; i++)
+				acc[i] = acc[i - 1] + data[i - 1];
+			sb.append(solve(0, K - 1)).append('\n');
 		}
-		
 		System.out.println(sb);			
+	}
+	public static long solve(int start, int end) {
+		if(start >= end) 
+			return 0;
+		if(dp[start][end] != -1)
+			return dp[start][end];
+		if(start + 1 == end) 
+			return data[start] + data[end];
+		dp[start][end] = Integer.MAX_VALUE;
+		for(int i = start; i < end; i++)
+			dp[start][end] = Math.min(dp[start][end], solve(start, i) + solve(i + 1, end) + acc[end + 1] - acc[start]);
+		return dp[start][end];
 	}
 }
