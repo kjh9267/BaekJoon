614c93fd54d7c9293244f34a824ffc3cb4157882
kjh9267 <kjh9267z@naver.com>
2019-06-29 15:32:55 +0900

    dynamic_programming

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * 
@@ -15,37 +15,15 @@ public class Test {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int num[] = new int[100];
-		int cnt = 1;
-		
-		Arrays.fill(num, 0);
-		for(int i = 97; i < 100; i++)
-			num[i] = 6;
-		
-		while(cnt < N) {
-			num[99] += 1;
-			for(int i = 99; i > 0; i--){
-				if(num[i] == 10){
-					num[i-1] += 1;
-					num[i] = 0;
-				}
-			}
-			for(int i = 0; i < 100; i++){
-				if(num[i] == 6 && num[i - 1] == 6 && num[i - 2] == 6){
-					cnt += 1;
-					break;
-				}
-			}
+
+		while(N-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append("You get ").append(a/b).append(" piece(s) and your dad gets ").append(a%b).append(" piece(s).").append('\n');
 		}
-		int pointer = 0;
-		for(int i = 0; i < 100; i++) {
-			if(num[i] != 0) {
-				pointer = i;
-				break;
-			}
-		}
-		for(int i = pointer; i < 100; i++)
-			sb.append(num[i]);
+		
 		System.out.println(sb);			
 	}
 }
---
+++ src/dynamic_programming/BOJ_11726.java
@@ -0,0 +1,31 @@
+package dynamic_programming;
+
+/**
+ * @author Junho
+ * 
+ * @see https://www.acmicpc.net/problem/11726
+ * 
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_11726 {
+	public static int[] dp;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		dp = new int[N + 1];
+		System.out.println(solve(N));
+	}
+	
+	public static int solve(int N) {
+		if(N <= 1)
+			return 1;
+		if(dp[N] != 0)
+			return dp[N];
+		dp[N] = solve(N - 1) % 10_007 + solve(N - 2) % 10_007;
+		return dp[N] % 10_007;
+	}
+}
