ddc11e7acff1d36068a0f975ccd45e7c886ba89a
kjh9267 <kjh9267z@naver.com>
2019-06-30 21:54:57 +0900

    breadth_first_search

--- src/Test.java
+++ src/Test.java
@@ -1,49 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-/**
- * 
- * @author Junho
- *
- * @see https://www.acmicpc.net/problem/11055
- *
- */
 
 public class Test {
-	public static int[] sequence, dp, res;
-	public static int N;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sequence = new int[N];
-		dp = new int[N];
-		int res = 0;
 		
-		Arrays.fill(dp, -1);
-		for(int i = 0; i < N; i++)
-			sequence[i] = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		if ((N % 4 == 0 && N % 100 != 0) || N % 400 == 0)
+			System.out.println(1);
+		else
+			System.out.println(0);
 		
-		for(int i = 0; i < N; i++)
-			res = Math.max(res, solve(i));
 		
-		System.out.println(res);
-	}
-	
-	public static int solve(int start) {
-		if(dp[start] != -1)
-			return dp[start];
-		
-		dp[start] = 1;
-		for(int next = start + 1; next < N; next++)
-			if(sequence[start] < sequence[next]) {
-				dp[start] = Math.max(dp[start], solve(next) + 1);
-				res[next]	
-			}
-
-		return dp[start];
 	}
 }
---
+++ src/breadth_first_search/BOJ_16469.java
@@ -0,0 +1,86 @@
+package breadth_first_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author Junho
+ *
+ * @see https://www.acmicpc.net/problem/16469
+ *
+ */
+
+public class BOJ_16469 {
+	public static int R, C, res;
+	public static char[][] grid;
+	public static Queue<Node> queue = new LinkedList<Node>();
+	public static int[][] visited;
+	public static final int[][] DIR = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		grid = new char[R][C];
+		visited = new int[R][C];
+		
+		for(int i = 0; i < R; i++)
+			grid[i] = br.readLine().toCharArray();
+		
+		for(int i = 0; i < 3; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			queue.add(new Node(y - 1, x - 1, i));
+			visited[x - 1][y - 1] |= (1 << i);
+		}
+		int cnt = bfs();
+		System.out.println(cnt == -1 ? -1 : cnt + " " + res);
+	}
+	
+	public static class Node{
+		int x, y, id;
+		public Node(int x, int y, int id) {
+			this.x = x;
+			this.y = y;
+			this.id = id;
+		}
+	}
+	
+	public static int bfs() {
+		int cnt = 0;
+		boolean key = false;
+		while(!queue.isEmpty()) {
+			int len = queue.size();
+			cnt += 1;
+			while(len-- > 0) {
+				Node cur = queue.poll();
+				for(int[] dir : DIR) {
+					int nextX = cur.x + dir[0];
+					int nextY = cur.y + dir[1];
+					if(0 > nextX || nextX >= C || 0 > nextY || nextY >= R)
+						continue;
+					if((visited[nextY][nextX] & (1 << cur.id)) != 0)
+						continue;
+					if(grid[nextY][nextX] == '1')
+						continue;
+					visited[nextY][nextX] |= (1 << cur.id);
+					if(visited[nextY][nextX] == 7) {
+						key = true;
+						res += 1;
+					}
+					queue.add(new Node(nextX, nextY, cur.id));
+				}
+			}
+			if(key)
+				return cnt;
+		}
+		return -1;
+	}
+}
