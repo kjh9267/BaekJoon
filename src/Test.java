3724ed1b485871875df397ab8b4cff482ce8abb8
kjh9267 <kjh9267z@naver.com>
2019-07-03 20:42:03 +0900

    tree_search

--- src/Test.java
+++ src/Test.java
@@ -6,12 +6,6 @@ public class Test {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
-		if ((N % 4 == 0 && N % 100 != 0) || N % 400 == 0)
-			System.out.println(1);
-		else
-			System.out.println(0);
-		
-		
+		System.out.println((int) -1.4);
 	}
 }
---
+++ src/depth_first_search/BOJ_1991.java
@@ -0,0 +1,70 @@
+package depth_first_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author Junho
+ *
+ * @see https://www.acmicpc.net/problem/1991
+ *
+ */
+
+public class BOJ_1991 {
+	public static Node[] tree;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		tree = new Node[26];
+		
+		while(N-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int cur = st.nextToken().charAt(0) - 'A';
+			int left = st.nextToken().charAt(0) - 'A';
+			int right = st.nextToken().charAt(0) - 'A';
+			tree[cur] = new Node(left, right);
+		}
+		preorder(0);
+		sb.append('\n');
+		inorder(0);
+		sb.append('\n');
+		postorder(0);
+		System.out.println(sb);
+	}
+	
+	public static class Node{
+		int left, right;
+		public Node(int left, int right) {
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	public static void preorder(int cur) {
+		if(cur < 0)
+			return;
+		sb.append((char) (cur + 65));
+		preorder(tree[cur].left);
+		preorder(tree[cur].right);
+	}
+	
+	public static void inorder(int cur) {
+		if(cur < 0)
+			return;
+		inorder(tree[cur].left);
+		sb.append((char) (cur + 65));
+		inorder(tree[cur].right);
+	}
+	
+	public static void postorder(int cur) {
+		if(cur < 0)
+			return;
+		postorder(tree[cur].left);
+		postorder(tree[cur].right);
+		sb.append((char) (cur + 65));
+	}
+}
