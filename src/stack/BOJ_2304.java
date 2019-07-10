907c23be48fa2c488b5586b6a2ba9ae269504e81
kjh9267 <kjh9267z@naver.com>
2019-07-10 19:04:53 +0900

    stack

---
+++ src/stack/BOJ_2304.java
@@ -0,0 +1,68 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * @author Junho
+ *
+ * @see https://www.acmicpc.net/problem/2304
+ *
+ */
+
+public class BOJ_2304 {
+	public static Stick[] data;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		data = new Stick[N];
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			data[i] = new Stick(x, y);
+		}
+		Arrays.sort(data);
+		System.out.println(solve(N));
+	}
+	
+	public static int solve(int N) {
+		int res = 0;
+		Stack<Stick> stack = new Stack<Stick>();
+		stack.push(new Stick(data[0].x, 0));
+		for(int i = 0; i < N; i++) {
+			if(stack.peek().y < data[i].y) {
+				Stick s = stack.pop();
+				stack.push(data[i]);
+				res += s.y * (data[i].x - s.x);
+			}
+		}
+		res += stack.pop().y;
+		stack.push(new Stick(data[N - 1].x, 0));
+		for(int i = N - 1; i >= 0; i--) {
+			if(stack.peek().y <= data[i].y) {
+				Stick s = stack.pop();
+				res += s.y * (s.x - data[i].x);
+				stack.push(data[i]);
+			}
+		}
+		return res;
+	}
+	
+	public static class Stick implements Comparable<Stick>{
+		int x, y;
+		public Stick(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		@Override
+		public int compareTo(Stick o) {
+			return this.x - o.x;
+		}
+	}
+}
