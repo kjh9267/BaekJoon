921413cebb1e6381292951c00c6623b78283e79b
kjh9267 <kjh9267z@naver.com>
2019-07-30 21:47:32 +0900

    binary_indexed_tree

--- src/Test.java
+++ src/Test.java
@@ -1,54 +1,13 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.InputMismatchException;
 public class Test {
-	public static long[] tree;
 	public static int N;
 	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		IR in = new IR(System.in);
 		N = in.rI();
-		tree = new long[N + 1];
-		for(int i = 1; i <= N; i++) {
-			int num = in.rI();
-			update(i, num);
-			update(i + 1, -num);
-		}
-		System.out.println(Arrays.toString(tree));
-		int Q = in.rI();
-		while(Q-- > 0) {
-			int op = in.rI();
-			if(op == 1) {
-				int s = in.rI();
-				int e = in.rI();
-//				update(s, 1);
-//				update(e + 1, -(e-s+1)*(e-s+2)/2);
-				long cnt = 1;
-				for(int i = s; i <= e;i ++) {
-					update(i,1);
-//					update(i+1,-cnt++);
-				}
-			}
-			else
-				sb.append(get(in.rI())).append('\n');
-		}
-		System.out.println(Arrays.toString(tree));
-		System.out.println(sb);
-	}
-	public static void update(int idx, long value) {
-		while(idx < N + 1) {
-			tree[idx] += value;
-			idx += (-idx & idx);
-		}
-	}
-	public static long get(int idx) {
-		long acc = 0;
-		while(idx > 0) {
-			acc += tree[idx];
-			idx -= -idx & idx;
-		}
-		return acc;
+
 	}
 	private static class IR {
 		private InputStream stream;
