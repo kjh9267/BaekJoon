db12ae6d841bcf89fb89fe058daea38640382a52
kjh9267 <kjh9267z@naver.com>
2019-08-12 22:38:40 +0900

    back_tracking

---
+++ src/back_tracking/BOJ_1342.java
@@ -0,0 +1,62 @@
+package back_tracking;
+
+// https://www.acmicpc.net/problem/1342
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class BOJ_1342 {
+    public static int N, res;
+    public static char[] data;
+    public static boolean[] visited;
+    public static HashMap<Character, Integer> cnt;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        data = br.readLine().toCharArray();
+        N = data.length;
+        visited = new boolean[N];
+        cnt = new HashMap<>();
+
+        for(int i = 0; i < N; i++){
+            if(cnt.containsKey(data[i]))
+                cnt.put(data[i], cnt.get(data[i]) + 1);
+            else
+                cnt.put(data[i], 1);
+        }
+        dfs(0, ' ', new char[N]);
+        for(int c : cnt.values()){
+            for(int i = 1; i <= c; i++){
+                res /= i;
+            }
+        }
+        System.out.println(res);
+    }
+    public static void dfs(int depth, char prev, char[] s){
+        if(depth == N && check(s)){
+                res += 1;
+                return;
+        }
+        for(int i = 0; i < N; i++){
+            if(prev == data[i])
+                continue;
+            if(visited[i])
+                continue;
+            visited[i] = true;
+            char temp = s[depth];
+            s[depth] = data[i];
+            dfs(depth + 1, data[i], s);
+            s[depth] = temp;
+            visited[i] = false;
+        }
+
+    }
+    public static boolean check(char[] s){
+        for(int i = 0; i < N - 1; i++){
+            if(s[i] == s[i + 1])
+                return false;
+        }
+        return true;
+    }
+}
