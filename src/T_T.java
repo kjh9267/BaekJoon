adcd5d8cff92eae7e1c7ed37cabd2709105b7c75
kjh9267 <kjh9267z@naver.com>
2019-08-16 20:22:10 +0900

    dfs

--- src/T_T.java
+++ src/T_T.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class T_T {
     public static void main(String[] args) throws Exception{
---
+++ src/depth_first_search/BOJ_3584.java
@@ -0,0 +1,45 @@
+package depth_first_search;
+
+// https://www.acmicpc.net/problem/3584
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3584 {
+    private static int[] adj;
+    private static boolean[] visited;
+    public static int res;
+
+    public static void main(String[] args) throws Exception{
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0){
+            int N = Integer.parseInt(br.readLine());
+            adj = new int[N + 1];
+            visited = new boolean[N + 1];
+            for(int i = 0; i < N - 1; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                adj[B] = A;
+            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            dfs(x);
+            dfs(y);
+            sb.append(res).append('\n');
+        }
+        System.out.print(sb);
+    }
+    public static void dfs(int cur){
+        if(visited[cur]) {
+            res = cur;
+            return;
+        }
+        visited[cur] = true;
+        dfs(adj[cur]);
+    }
+}
