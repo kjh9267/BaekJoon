a697d99c66d7563f08e147c43332c6f9cf0e6d01
kjh9267 <kjh9267z@naver.com>
2020-01-12 18:28:33 +0900

    BIT

---
+++ src/binary_indexed_tree/BOJ_2268.java
@@ -0,0 +1,60 @@
+package binary_indexed_tree;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2268
+
+public class BOJ_2268 {
+    public static long[] tree, data;
+    public static final String NEW_LINE = "\n";
+    public static int N;
+
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        tree = new long[N + 1];
+        data = new long[N + 1];
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(op == 1) {
+                long diff = b - data[a];
+                data[a] = b;
+                update(a, diff);
+            }
+            else if (a < b) {
+                sb.append(sum(b) - sum(a - 1)).append(NEW_LINE);
+            }
+            else{
+                sb.append(sum(a) - sum(b - 1)).append(NEW_LINE);
+
+            }
+        }
+        System.out.print(sb.toString());
+    }
+
+    public static void update(int idx, long diff) {
+        while(idx < N + 1) {
+            tree[idx] += diff;
+            idx += (-idx & idx);
+        }
+    }
+
+    public static long sum(int idx) {
+        long res = 0;
+        while(idx > 0) {
+            res += tree[idx];
+            idx -= (-idx & idx);
+        }
+        return res;
+    }
+}
