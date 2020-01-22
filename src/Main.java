c9640bb4734f8bc1907d2a4495ce4e5da2016c43
kjh9267 <kjh9267z@naver.com>
2020-01-22 20:12:15 +0900

    simulation

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Map<String,Integer> table = new HashMap<>();
-
-        for(int i = 0; i < N; i++) {
-            String word = br.readLine();
-            if (table.containsKey(word))
-                table.put(word, table.get(word) + 1);
-            else
-                table.put(word, 1);
-        }
-
-        ArrayList<Pair> list = new ArrayList<>();
-
-
-        for(String word: table.keySet())
-            list.add(new Pair(word, table.get(word)));
-
-        Collections.sort(list);
-
-        System.out.println(list.get(0).word);
-    }
-
-    public static class Pair implements Comparable<Pair>{
-        String word;
-        int cnt;
-
-        public Pair(String word, int cnt){
-            this.word = word;
-            this.cnt = cnt;
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            if(this.cnt == o.cnt)
-                return this.word.compareTo(o.word);
-            return o.cnt - this.cnt;
-        }
     }
 }
---
+++ src/simulation/BOJ_1091.java
@@ -0,0 +1,64 @@
+package simulation;
+
+// https://www.acmicpc.net/problem/1091
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1091 {
+    private static final int MAX = 1_000_000;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] target = new int[N];
+        int[] data = new int[N];
+        int[] shuffleTable = new int[N];
+        int cnt = 0;
+
+        for(int idx = 0; idx < N; idx++)
+            target[idx] = idx % 3;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int idx = 0; idx < N; idx++)
+            data[idx] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int idx = 0; idx < N; idx++)
+            shuffleTable[idx] = Integer.parseInt(st.nextToken());
+
+        while (!end(N, data, target)) {
+            shuffle(N, data, shuffleTable);
+            cnt += 1;
+
+            if (cnt == MAX){
+                cnt = -1;
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static boolean end(int N, int[] data, int[] target){
+
+        for(int idx = 0; idx < N; idx++)
+            if(data[idx] != target[idx])
+                return false;
+
+        return true;
+    }
+
+    public static void shuffle(int N, int[] data, int[] shuffleTable){
+        int[] temp = new int[N];
+
+        for(int idx = 0; idx < N; idx++) {
+            int nextIdx = shuffleTable[idx];
+            temp[nextIdx] = data[idx];
+        }
+
+        for(int idx = 0; idx < N; idx++)
+            data[idx] = temp[idx];
+    }
+}
