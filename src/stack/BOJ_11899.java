53bb69ff11c53a37494e3f128379d2a050dcc08a
kjh9267 <kjh9267z@naver.com>
2020-01-28 20:24:24 +0900

    Stack

--- src/stack/BOJ_11899.java
+++ src/stack/BOJ_11899.java
@@ -1,10 +1,12 @@
+package stack;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 // https://www.acmicpc.net/problem/11899
 
-public class Main {
+public class BOJ_11899 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String data = br.readLine();
