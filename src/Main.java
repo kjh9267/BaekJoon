446d0639a8b69e32b852cbdb170ff7de9618cff5
kjh9267 <kjh9267z@naver.com>
2020-01-28 21:51:53 +0900

    Stack

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
-
-// https://www.acmicpc.net/problem/11899
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String data = br.readLine();
-        Stack<Character> stack = new Stack<>();
-        int N = data.length();
 
-        for(int idx = 0; idx < N; idx++){
-            char value = data.charAt(idx);
-            if(value == ')' && !stack.isEmpty() && stack.peek() == '(')
-                stack.pop();
-            else
-                stack.push(value);
-        }
+    public static void main(String[] args) throws Exception {
 
-        System.out.println(stack.size());
     }
 }
---
+++ src/stack/BOJ_2493.java
@@ -0,0 +1,58 @@
+package stack;
+
+// https://www.acmicpc.net/problem/2493
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_2493 {
+    private static final String SPACE = " ";
+
+    public static void main(String[] args) throws Exception{
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] data = new int[N + 1];
+        int[] res = new int[N + 1];
+        Stack<Pair> stack = new Stack<>();
+
+        for(int idx = 1; idx < N + 1; idx++)
+            data[idx] = Integer.parseInt(st.nextToken());
+
+        for(int idx = N; idx >= 1; idx--){
+            int value = data[idx];
+            while (!stack.isEmpty()){
+                Pair top = stack.peek();
+                if(value < top.value)
+                    break;
+                res[top.idx] = idx;
+                    stack.pop();
+            }
+            stack.push(new Pair(idx, value));
+        }
+
+        while (!stack.isEmpty()){
+            Pair top = stack.pop();
+            res[top.idx] = 0;
+        }
+
+        for(int idx = 1; idx < N + 1; idx++)
+            sb.append(res[idx]).append(SPACE);
+
+        System.out.print(sb);
+    }
+
+    public static class Pair{
+        int idx;
+        int value;
+
+        public Pair(int idx, int value){
+            this.idx = idx;
+            this.value = value;
+        }
+    }
+}
