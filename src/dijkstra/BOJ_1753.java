55008ba0ed438f9d35d12ee3ff7cf7f8fff79f14
kjh9267 <kjh9267z@naver.com>
2020-06-30 21:44:46 +0900

    Refactor: dijkstra

--- src/dijkstra/BOJ_1753.java
+++ src/dijkstra/BOJ_1753.java
@@ -1,5 +1,7 @@
 package dijkstra;
 
+// https://www.acmicpc.net/problem/1753
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -7,15 +9,39 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-import dijkstra.BOJ_1753.Node;
-
 public class BOJ_1753 {
 
-	public static ArrayList<Node>[] graph = null;
 	public static final int INF = 200_000_000;
-	public static int[] dist;
 
-	public static class Node implements Comparable<Node> {
+	public static void main(String args[]) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
+
+		ArrayList<Node>[] graph = new ArrayList[V + 1];
+
+		for (int node = 1; node <= V; node++) {
+			graph[node] = new ArrayList<>();
+		}
+
+		for (int loop = 0; loop < E; loop++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+
+			graph[from].add(new Node(to, cost));
+		}
+
+		System.out.println(dijkstra(V, K, graph));
+
+	}
+
+	private static class Node implements Comparable<Node> {
 		int node;
 		int cost;
 
@@ -25,73 +51,38 @@ public class BOJ_1753 {
 		}
 
 		@Override
-		public int compareTo(Node n) {
-			if (this.cost > n.cost) {
-				return 1;
-			} 
-			else if (this.cost == n.cost) {
-				return 0;
-			} 
-			else {
-				return -1;
-			}
-
+		public int compareTo(Node other) {
+			return this.cost - other.cost;
 		}
 	}
 
-	public static void dijkstra(int start) {
+	private static StringBuilder dijkstra(int V, int start, ArrayList<Node>[] graph) {
+		StringBuilder sb = new StringBuilder();
 		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		
+		int[] dist = new int[V + 1];
+
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+
 		pq.add(new Node(start, dist[start]));
 
 		while (!pq.isEmpty()) {
 			int cur = pq.poll().node;
 			
 			for(Node next : graph[cur]) {
-				if(dist[next.node] > dist[cur] + next.cost) {
-					dist[next.node] = dist[cur] + next.cost;
-					pq.add(new Node(next.node,dist[next.node]));
+				if(dist[next.node] <= dist[cur] + next.cost) {
+					continue;
 				}
+				dist[next.node] = dist[cur] + next.cost;
+				pq.add(new Node(next.node,dist[next.node]));
 			}
 		}
 
+		for(int node = 1; node <= V; node++) {
+			sb.append(dist[node] == INF ? "INF" : dist[node]).append("\n");
+		}
+
+		return sb;
 	}
 
-	public static void main(String args[]) throws Exception {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(br.readLine());
-
-		graph = new ArrayList[V + 1];
-
-		for (int i = 0; i < V + 1; i++) {
-			graph[i] = new ArrayList<>();
-		}
-
-		dist = new int[V + 1];
-		Arrays.fill(dist, INF);
-		dist[K] = 0;
-
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-
-			graph[from].add(new Node(to, cost));
-		}
-		
-		dijkstra(K);
-		
-		for(int i = 1; i < V+1; i++) {
-			sb.append(dist[i] == INF ? "INF" : dist[i]).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
 }
